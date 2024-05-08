728dabec19c5cf88c974b0f7288d3db9236211b2
Murilo Bagodi <murilosbagodi@hotmail.com>
2024-05-08 09:40:05 -0300

    feat/RegEx-Routes -> Developing routes using regex to make more performatic routes

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 import http from "node:http";
-
+import { routes } from "./src/middlewares/routes.js";
+import { json } from "./src/middlewares/Json.js";
 const server = http.createServer(async (req, res) => {
-  const routes = routes.find(route => {
-    return route.method === method && route.path
-  })
+  const {method, url} = req
+
+  await json(req,res)
+  const route = routes.find((route) => {
+    return route.method === method && route.path.test(url)
+  });
+
+  if(route){
+    const routeParams = req.url.match(route.path)
+    return route.handler(req, res)
+  }
+
+  res.writeHead(404).end()
 });
 
 server.listen(3333);
--- src/middlewares/routes.js
+++ src/middlewares/routes.js
@@ -1,30 +1,38 @@
-import { Database } from "./database";
-const database = new Database()
+import { buildRoutePath } from "../build/build-route-path.js";
+import { Database } from "./database.js";
+const database = new Database();
 export const routes = [
   {
     method: "GET",
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: (req, res) => {
       const users = database.select("users");
 
-      return res.end(JSON.stringify(users))
+      return res.end(JSON.stringify(users));
     },
   },
   {
     method: "POST",
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: (req, res) => {
       const { nome, email } = req.body;
 
-      const user ={
+      const user = {
         nome,
         email,
         id: randomUUID(),
       };
 
-      database.insert("users", user)
+      database.insert("users", user);
 
       res.writeHead(201).end();
+    },
+  },
+  {
+    method: "DELETE",
+    path: buildRoutePath("/users/:id"),
+    handler: (req, res) => {
+      return res.end()
     },
   },
 ];
